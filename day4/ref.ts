a1227c989a6f48f697dd9bed0a3bb4c38245c7ab
remedeae <sanna.m.blomdahl@gamail.com>
2025-09-07 23:51:32 +0200

    reworked the promise assignment into an async function in the wild-magic-app. expanded on it to accomodate searching for spells, made html and css to display and interact with teh function.

--- day4/ref.ts
+++ day4/ref.ts
@@ -9,40 +9,80 @@ type spellList = {
    results: spellInfo[],
 }
 
-const fetchSpell = (castSpellOutcome: unknown) => {
-   const promise: Promise<void> = fetch(
-      "https://www.dnd5eapi.co/api/2014/spells")
-      .then((response : Response) => {
-         if (!response.ok) {
-            throw new Error("Failure retreiveing data.");
-         }
-         return response.json() as Promise<spellList>;
-      })
-      .then((data : spellList) => {
-         //   console.log(data)
-         const spellID: number = Math.floor(Math.random() * data.count) - 1;
-         const spell: string = data.results[spellID]!.name;
-         console.log(castSpellOutcome + spell);
-      })
-      .catch((error : unknown) => {
-         console.error("Error fetching spell: ", error);
-      })
+const fetchSpellData = async (spellURL: string) => {
+   try {
+      const response = await fetch(`https://www.dnd5eapi.co${spellURL}`);
+      const data = await response.json();
+      const spellEffect = data.damage !== undefined ?
+         `which deals ${data.damage.damage_type.index} damage to your foe. The orc perisches and you emerge victorious.` :
+         "which distracts your foe and you barely manages to escape. You may live another day, but somewhere out there an orc is waiting for his time to strike you down.";
+      return spellEffect;
+
+   } catch (error) {
+      console.log("Error fetching spell details: ", error)
+   }
 }
-const castSpell = new Promise((resolve, reject) => {
 
-   let outcome: number = Math.random();
+const fetchSpell = async () => {
 
-   outcome > 0.5 ?
-      resolve("You cast ") :
-      reject("Your spell fizzels.");
-});
-castSpell
-   .then((message) => {
-      fetchSpell(message);
+   try {
+      const response = await fetch("https://www.dnd5eapi.co/api/2014/spells");
+      const data: spellList = await response.json();
+      const spellID: number = Math.floor(Math.random() * data.count) - 1;
+      const spell: string = data.results[spellID]!.name;
+      const damage = await fetchSpellData(data.results[spellID]!.url);
+      return `${spell} ${damage}`;
+
+   } catch (error) {
+      console.error("Error fetching spell: ", error);
+   }
+}
+const rollSpellSuccess = () => {
+   return new Promise((resolve, reject) => {
+      const outcome: number = Math.random() * 4;
+      //console.log(outcome)
+      outcome >= 1 ?
+         resolve("You cast") :
+         reject("Your spell fizzels.");
    })
-   .catch((error) => {
+};
+const castSpell = async () => {
+   try {
+      const spellSuccess = await rollSpellSuccess();
+      const fetchedSpell = await fetchSpell();
+      const message: string = `${spellSuccess} ${fetchedSpell}`;
+      console.log(message);
+   } catch (error) {
       console.log(error)
-   });
+   }
+}
+//fetchSpell();
+//castSpell()
+
+const fetchSpellRequested = async (requestedSpell: string) => {
+   try {
+      const response = await fetch("https://www.dnd5eapi.co/api/2014/spells");
+      const data: spellList = await response.json();
+      const spellObject = data.results.filter(item => item.name.toLowerCase() === requestedSpell.toLowerCase());
+      const damage = await fetchSpellData(spellObject[0]!.url)
+      const spell = spellObject[0]!.name;
+      console.log(`${spell} ${damage}`);
+      return `${spell} ${damage}`;
+
+
+   } catch (error) {
+      console.error("Error fetching spell: ", error);
+   }
+}
+
+fetchSpellRequested("light");
+
+
+
+
+
+
+
 
 //orc sprites https://free-game-assets.itch.io/free-top-down-orc-game-character-pixel-art
 //DnD 5e API: https://www.dnd5eapi.co/api/2014/
--- day4/wild-magic-app/src/App.tsx
+++ day4/wild-magic-app/src/App.tsx
@@ -1,8 +1,85 @@
+import { useState } from "react";
+import orc from './assets/orc.svg'
 
 
 function App() {
 
-let combatMesage : string = "PANICC!"
+  type spellInfo = {
+    index: string,
+    name: string,
+    level: number,
+    url: string,
+  }
+  type spellList = {
+    count: number,
+    results: spellInfo[],
+  }
+
+  const [combatMessage, setCombatMessage] = useState("After a long battle you are exhausted, yet before you still stands a final towering orc, he's just as tired as you and just wants to go home. As he raises his blade to finish you off you feel a final spark of magic emerging from deep within you.");
+  const [chooseSpell, setChooseSpell] = useState("");
+
+  const fetchSpellData = async (spellURL: string) => {
+    try {
+      const response = await fetch(`https://www.dnd5eapi.co${spellURL}`);
+      const data = await response.json();
+      const spellEffect = data.damage !== undefined ?
+        `which deals ${data.damage.damage_type.index} damage to your foe. The orc perisches and you emerge victorious.` :
+        "which distracts your foe and you barely manages to escape. You may live another day, but somewhere out there an orc is waiting for his time to strike you down.";
+      return spellEffect;
+
+    } catch (error) {
+      console.log("Error fetching spell details: ", error)
+    }
+  }
+
+  const fetchSpell = async () => {
+
+    try {
+      const response = await fetch("https://www.dnd5eapi.co/api/2014/spells");
+      const data: spellList = await response.json();
+      const spellID: number = Math.floor(Math.random() * data.results.length);
+      const spell: string = data.results[spellID]!.name;
+      const damage = await fetchSpellData(data.results[spellID]!.url);
+      return `"${spell}" ${damage}`;
+
+    } catch (error) {
+      console.error("Error fetching spell: ", error);
+    }
+  }
+  const rollSpellSuccess = () => {
+    return new Promise((resolve, reject) => {
+      const outcome: number = Math.random() * 4;
+      //console.log(outcome)
+      outcome >= 1 ?
+        resolve("You cast") :
+        reject("Your spell fizzles. The orc laughs manically at you as his blade rips your flesh and life fades from your veins .");
+    })
+  };
+  const castSpell = async () => {
+    try {
+      const spellSuccess = await rollSpellSuccess();
+      const fetchedSpell = await fetchSpell();
+      const message: string = `${spellSuccess} ${fetchedSpell}`;
+      setCombatMessage(message);
+    } catch (error) {
+
+      setCombatMessage(`${error}`);
+    }
+  };
+  const fetchSpellRequested = async (requestedSpell: string) => {
+    try {
+      const response = await fetch("https://www.dnd5eapi.co/api/2014/spells");
+      const data: spellList = await response.json();
+      const spellObject = data.results.filter(item => item.name.toLowerCase() === requestedSpell.toLowerCase());
+      const damage = await fetchSpellData(spellObject[0]!.url)
+      const spell = spellObject[0]!.name;
+      const outcome: string = `${spell} ${damage}`;
+      //console.log(`${spell} ${damage}`);
+      setCombatMessage(`You cast ${outcome}`);
+    } catch (error) {
+      setCombatMessage("Hubris! You did not know your spells and now you suffer a grusome death at the hands of this orc.");
+    }
+  }
 
   return (
     <div className="home">
@@ -13,12 +90,17 @@ let combatMesage : string = "PANICC!"
           <i className="fa-solid fa-wand-sparkles"></i>
         </div>
         <div className="enemy">
-
+          <img src={orc} alt="Orc Enemy" />
         </div>
         <div className="combatLog">
-          <p>{combatMesage}</p>
+          <p>{combatMessage}</p>
         </div>
-        <button className="castSpellButton" /* onClick={castSpell} */>Unleash the magic!</button>
+        <button className="castSpellButton" onClick={castSpell} >Unleash the magic!</button>
+        <label htmlFor="chooseSpell">HA! I'm no random spellcaster, I cast;</label>
+        <div>
+          <input id="chooseSpell" type="text" placeholder="Enter spell name here" value={chooseSpell} onChange={(e) => setChooseSpell(e.target.value)} />
+          <button className="chooseCastButton" onClick={() => fetchSpellRequested(chooseSpell)}>Cast!</button>
+        </div>
       </div>
     </div>
   )
